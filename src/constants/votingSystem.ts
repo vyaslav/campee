f09a10db7d368a3782737c233478d34ae38f98a3
Viacheslav Avramenko <vyaslav@gmail.com>
2026-03-18 21:45:07 +0100

    Change sequential voting values to start from 0
    Updated the sequential voting values to start from 0.

--- src/constants/votingSystem.ts
+++ src/constants/votingSystem.ts
@@ -4,7 +4,7 @@ export const VOTING_SYSTEM_VOTE_VALUES: Record<VotingSystem, string[]> = {
   scrum: ["0", "0.5", "1", "2", "3", "5", "8", "13", "20"],
   // eslint-disable-next-line sort-keys
   fibonacci: ["0", "1", "2", "3", "5", "8", "13", "21", "34"],
-  sequential: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+  sequential: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
   "t-shirts": ["xs", "s", "m", "l", "xl"],
 };
 
